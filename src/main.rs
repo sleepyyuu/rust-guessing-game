5c775b3949068959f9def6df88b88826a15c014f
Yu <k28143995@gmail.com>
2022-06-28 14:31:30 -0700

    Detailing comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin() //can use std::io::stdin if no use std::io
+        .read_line(&mut guess)  //mutable reference to guess
+                                //.read_line returns a value, io::Result, that is a enum
+                                //either "ok" or "err"
+        .expect("Failed to read line");  //.expect handles "err" enum
+
+    println!("You guessed: {}", guess);  //{} is placeholder, guess variable goes in there
 }
